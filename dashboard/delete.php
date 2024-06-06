ad7da1d8c41174248c53c53bcfd974bd30e125b5
Javohyr <fstudioadm@gmail.com>
2024-06-06 15:22:15 +0500

    del product fix

--- dashboard/delete.php
+++ dashboard/delete.php
@@ -4,32 +4,57 @@ session_start();
 
 require '../connect/db.php';
 
+// Check if user is logged in
 if (!isset($_SESSION['user'])) {
     header('Location: log.php');
+    exit();
 }
 
+// Check if id is set
 if (!isset($_GET['id'])) {
     header('Location: index.php');
+    exit();
 }
 
-$product = R::load('product', $_GET['id']);
+// Sanitize inputs
+$id = intval($_GET['id']);
 
-if ($product['type'] == 'page') {
-    $page = R::findOne('page', 'parent_id = ?', [$product['id']]);
-    $page = R::load('page', $page['id']);
-    R::trash($page);
-}
+// Recursive function to delete a product and its child products
+function deleteProduct($productId) {
+    // Load the product
+    $product = R::load('product', $productId);
 
-R::trash($product);
+    if ($product->id) {
+        // If the product is of type 'page', delete associated page
+        if ($product->type == 'page') {
+            $page = R::findOne('page', 'parent_id = ?', [$product->id]);
+            if ($page) {
+                R::trash($page);
+            }
+        }
 
-$child_products = R::find('product', 'parent_id = ?', [$_GET['id']]);
+        // Find and delete all child products
+        $child_products = R::find('product', 'parent_id = ?', [$productId]);
+        foreach ($child_products as $child_product) {
+            deleteProduct($child_product->id);
+        }
 
-if ($child_products) {
-    foreach ($child_products as $child_product) {
-        header('Location: delete.php?id=' . $child_product['id']);
+        // Delete the product
+        R::trash($product);
     }
-} else {
-    header('Location: index.php');
 }
+
+// Get the parent_id before deleting the main product
+$product = R::load('product', $id);
+$parentProductId = $product->parent_id;
+
+// Delete the main product
+deleteProduct($id);
+
+// Set the changedProductId in session
+$_SESSION['changedProductId'] = $parentProductId;
+
+// Redirect to index
+header('Location: index.php');
 
 ?>
